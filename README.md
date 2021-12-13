2ebb5fe248b8b3ff1c3885ecb3245c0d7897ce53
Mircea Romanitan <>
2021-12-14 01:32:57 +0200

    Added S5 area, changed the way map loads

--- README.md
+++ README.md
@@ -5,6 +5,53 @@ OSM interactive map with repeaters in YO area, and not only.
 
 ![alt text](https://github.com/motorox/repeaters-map/blob/master/repeaters-map.png?raw=true)
 
+Started from: https://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
 
 ## Used libraries and tools
 
+### Stuff used:
+- [leaflet](https://leafletjs.com/)<br/>
+- [icons](https://github.com/pointhi/leaflet-color-markers)<br/>
+- [YO db file](https://github.com/yo6nam/yodb)<br/>
+- [qth-locator](https://github.com/jleh/qth-locator)<br/>
+- [leaflet.markercluster](https://github.com/Leaflet/Leaflet.markercluster)<br/>
+- [reverse geocoding](https://github.com/osm-search/Nominatim)<br/>
+
+### DBs from internet:
+From the below links i made the JSON files and/or the JS markers arrays:
+      <p>http://ha2to.orbel.hu/content/repeaters/en/index.html</p>
+      <p>https://www.repeaterbook.com/row_repeaters/Display_SS.php?state_id=HU&band=14&loc=%&call=%&use=%</p>
+      <p>https://repeaterbook.com/row_repeaters/Display_SS.php?state_id=RS&freq=14%&band6=%&loc=%&call=%&status_id=%&features=%&coverage=%&use=%</p>
+      <p>http://lea.hamradio.si/~s51kq/S5RPT.HTM</p>
+      <p>https://clatsopauxcomm.org/technical/vhf-uhf</p>
+
+### Tools:
+- [html-table-to-json](https://www.convertjson.com/html-table-to-json.htm)<br/>
+
+## History
+This is from the example found:
+    <div>
+      <p>An example of how to use <a href="https://leafletjs.com/" target="_blank">Leaflet</a>.
+      <p>From the article <a href="https://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap" target="_blank">Creating An Interactive Map With Leaflet and OpenStreetMap</a>
+      <p>Andy Maloney // <a href="https://asmaloney.com" target="_blank">asmaloney.com</a>
+      <br/>23 Jan 2014
+      <p>
+        <b>[06 May 2021]</b>        
+        <ul>
+          <li>Updated to Leaflet 1.7.1, and jQuery 3.6.0.</li>
+          <li>All files reformatted with <a href="https://prettier.io/">prettier</a>.</li>
+          <li>Use <i>https</i> for URLs.</li>
+        </ul>
+      </p>
+      <p><b>[08 Oct 2019]</b>
+        <ul>
+          <li>Updated to Leaflet 1.5.1 and jQuery 3.4.1.</li>
+          <li>Added <a href="https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity">Subresource Integrity</a> to CSS and CDN JavaScript.</li>
+          <li>Changed the marker file from .json to .js since it is actually a JavaScript file.</li>
+          <li>Added a new country to the list :-)</li>
+        </ul>
+      <p><b>[16 Jul 2016]</b> Switched to Leaflet 0.7.7.
+      <p><b>[12 Jul 2016]</b> MapQuest discontinued the tile set I was using, so I switched to OpenStreetMap’s tiles. Note that these are useful only for testing. They say on their site:
+        <blockquote>"Apart from very limited testing purposes, you should not use the tiles supplied by OpenStreetMap.org itself."</blockquote>
+      <p><b>[19 Apr 2014]</b> Added <i>http:</i> to the scheme-less ajax.googleapis.com URI so double-clicking this file to open in the browser works.
+    </div>
--- src/repeaters-leaf.js
+++ src/repeaters-leaf.js
@@ -1,4 +1,3 @@
-// See post: https://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
 
 var map = L.map('map', {
   center: [45.76, 24.0],
@@ -6,8 +5,6 @@ var map = L.map('map', {
   zoom: 6,
 });
 
-// var layer = new L.StamenTileLayer('watercolor');
-// map.addLayer(layer);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -63,9 +60,8 @@ var orangeIcon = new L.Icon({
   popupAnchor: [-1, -22],
   shadowSize: [26, 26]
 });
-var repeaters = {"<img src='https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png' /> <span class='my-layer-item'>My Layer</span>" : L.layerGroup()};
 
-function makePopup(node) {
+function makeYOPopup(node) {
   let popup = '<div><b><i>' + node.banda + '</i></b><br><b>' + node.name + '</b><br><div>' + node.description + '</div>' + '<ul style="padding-left: 8px;font-size: smaller;">';
   $.each(node, function (k, v) {
     if (k == 'name' || k == 'k_id' || k == 'banda' || k == 'description' || k == 'nameA' || k == 'online' || k == 'pl_in' || k == 'pl_out') {
@@ -77,31 +73,6 @@ function makePopup(node) {
   popup += '</ul></div>';
   return popup;
 }
-
-$.getJSON({
-  url: "https://raw.githubusercontent.com/yo6nam/yodb/main/all.json",
-  cache: true,
-  crossDomain: true,
-  crossOriginIsolated: false
-}, function (data) {
-  // placeYoMarkers(data);
-  // placeLocatorMarkers(fm_markers_hu, "FM-HU");
-  // placeLocatorMarkers(dmr_markers_hu, "DMR-HU");
-  // placeLocatorMarkers(dstar_markers_hu, "DSTAR-HU");
-  // placeLocatorMarkers(dstar_markers_hu, "C4FM-HU");
-
-  placeLocatorMarkers("YO-repeaters", mapYOFunc, Object.values(data.nodes).map((val) => val));
-  placeLocatorMarkers("HA-repeaters", mapHuFunc, fm_markers_hu, dmr_markers_hu, dstar_markers_hu,c4fm_marker_hu);
-  placeLocatorMarkers("YT-repeaters", mapYTFunc, markers_YT);
-
-  $.each(repeaters, function (key, val) {
-    map.addLayer(val, key);
-  });
-  L.control.layers(null, repeaters).addTo(map);
-
-});
-
-// const { locatorToLatLng, distance, bearingDistance, latLngToLocator } = require('qth-locator');
 
 console.log(locatorToLatLng('IO91wm')); // [51.521, -0.125]
 console.log(distance('IO91wm', 'KP20le')); // 1821.5 km
@@ -147,10 +118,6 @@ console.log(latLngToLocator(60.179, 24.945)); // KP21le
 
 // console.log(json);
 
-// import * as data from './FM-repeaters-HU.json';
-// const {name} = data;
-// console.log(name); // output 'testing'
-
 // $.getJSON("./FM-repeaters-HU.json", function(json) {
 //   console.log(json); // this will show the info it in firebug console
 // });
@@ -179,33 +146,40 @@ function placeYoMarkers(data) {
 }
 
 function placeLocatorMarkers(overlay, mapFunc, ...data) {
+  return new Promise(function (resolve, reject) {
+  var repeaters = {};
   if (!repeaters[overlay]) { repeaters[overlay] = L.markerClusterGroup({
-    showCoverageOnHover: false, chunkedLoading: true, maxClusterRadius: 10,
+    showCoverageOnHover: false,
+    // chunkedLoading: true,
+    maxClusterRadius: 10,
     iconCreateFunction: function(cluster) {
-      var children = cluster.getAllChildMarkers();
-      console.log(children);
-      var sum = children.length;
-      // for (var i = 0; i < children.length; i++) {
-      //   sum += children[i].feature.properties.total;
-      // }
+      return new L.DivIcon({ html: '<b>' + cluster.getChildCount() + '</b>' });
+    }
+    // iconCreateFunction: function(cluster) {
+    //   var children = cluster.getAllChildMarkers();
+    //   console.log(children);
+    //   var sum = children.length;
+    //   // for (var i = 0; i < children.length; i++) {
+    //   //   sum += children[i].feature.properties.total;
+    //   // }
 
-      var childCount = cluster.getChildCount()
+    //   var childCount = cluster.getChildCount()
 
-      var c = ' marker-cluster-';
-      if (childCount + sum <= 50) {
-        c += 'small';
-      } else if (childCount + sum <= 250) {
-        c += 'medium';
-      } else {
-        c += 'large';
-      }
+    //   var c = ' marker-cluster-';
+    //   if (childCount + sum <= 50) {
+    //     c += 'small';
+    //   } else if (childCount + sum <= 250) {
+    //     c += 'medium';
+    //   } else {
+    //     c += 'large';
+    //   }
 
-      return new L.DivIcon({
-        html: '<div><span>' + sum + '</span></div>',
-        className: 'marker-cluster marker-cluster-' + c,
-        iconSize: new L.Point(40, 40)
-      });
-    },
+    //   return new L.DivIcon({
+    //     html: '<div><span>' + sum + '</span></div>',
+    //     className: 'marker-cluster marker-cluster-' + c,
+    //     iconSize: new L.Point(40, 40)
+    //   });
+    // },
     // spiderfyShapePositions: function(count, centerPt) {
     //   var distanceFromCenter = 35,
     //       markerDistance = 45,
@@ -224,55 +198,77 @@ function placeLocatorMarkers(overlay, mapFunc, ...data) {
     // }
   }); }
   data.forEach(element => {
-    element.map(el => mapFunc(el, overlay));
+    element.map(el => mapFunc(el, overlay, repeaters));
   });
+  resolve(repeaters);
+});
 }
 
-mapYOFunc = function (val, overlay) {
-  console.log(val);
+function addLayers2Map(repeaters) {
+  $.each(repeaters, function (key, val) {
+    map.addLayer(val);
+  });
+  L.control.layers(null, repeaters).addTo(map);
+}
+
+mapYOFunc = function (val, overlay, repeaters) {
   var selectedIcon = blueIcon;
   if (val.overlay == "dstar") { selectedIcon = greenIcon; }
   if (val.overlay == "tetra") { selectedIcon = greyIcon; }
   if (val.overlay.includes("dmr")) { selectedIcon = orangeIcon; }
   if (val.functional == "inactiv") { selectedIcon = redIcon; }
   marker = L.marker([val.lat, val.lon], { icon: selectedIcon })
-    .bindPopup(makePopup(val));
+    .bindPopup(makeYOPopup(val));
     // .addTo(map)  
-  marker.addTo(repeaters[overlay]);
+  // marker.addTo(repeaters[overlay]);
+  repeaters[overlay].addLayer(marker);
 }
 
-mapHuFunc = function(val, overlay) {
-  // console.log(node);
-  // $.each(node, function (key, val) {
-  //   console.log(key);
-    console.log(val);
-    if(val["QTH Locator"]){
-      var marker;
-      var [lat, lon] = locatorToLatLng(val["QTH Locator"]);
-      var selectedIcon = blueIcon;
-      if (val.Mode.toLowerCase().includes("dstar")) { selectedIcon = greenIcon; }
-      if (val.Mode.toLowerCase().includes("tetra")) { selectedIcon = greyIcon; }
-      if (val.Mode.toLowerCase().includes("dmr")) { selectedIcon = orangeIcon; }
-      if (val.State == "inactive") { selectedIcon = redIcon; }
-      marker = L.marker([lat, lon], { icon: selectedIcon }).bindPopup(makePopup2(val)); // .addTo(map)
-      marker.addTo(repeaters[overlay]);
-    }
-  // });
-}
-
-mapYTFunc = function(val, overlay) {
-  console.log(val);
-  // var coords = ;
-    // console.log(coords);
+mapHuFunc = function(val, overlay, repeaters) {
+  if(val["QTH Locator"]){
     var marker;
-    var [lat, lon] = [ 44.81, 20.45]//getCoordsFromAddress2(val["Location"]);
+    var [lat, lon] = locatorToLatLng(val["QTH Locator"]);
     var selectedIcon = blueIcon;
-    if (val.Modes.toLowerCase().includes("dstar")) { selectedIcon = greenIcon; }
-    if (val.Modes.toLowerCase().includes("tetra")) { selectedIcon = greyIcon; }
-    if (val.Modes.toLowerCase().includes("dmr")) { selectedIcon = orangeIcon; }
+    if (val.Mode.toLowerCase().includes("dstar")) { selectedIcon = greenIcon; }
+    if (val.Mode.toLowerCase().includes("tetra")) { selectedIcon = greyIcon; }
+    if (val.Mode.toLowerCase().includes("dmr")) { selectedIcon = orangeIcon; }
+    if (val.State == "inactive") { selectedIcon = redIcon; }
+    marker = L.marker([lat, lon], { icon: selectedIcon }).bindPopup(makePopup2(val));
+    repeaters[overlay].addLayer(marker);
+  }
+}
+
+mapSiFunc = function(val, overlay, repeaters) {
+  if(val["LOC"]){
+    var marker;
+    var [lat, lon] = locatorToLatLng(val["LOC"]);
+    var selectedIcon = blueIcon;
+    // if (val.Mode.toLowerCase().includes("dstar")) { selectedIcon = greenIcon; }
+    // if (val.Mode.toLowerCase().includes("tetra")) { selectedIcon = greyIcon; }
+    // if (val.Mode.toLowerCase().includes("dmr")) { selectedIcon = orangeIcon; }
     // if (val.State == "inactive") { selectedIcon = redIcon; }
-    marker = L.marker([lat, lon], { icon: selectedIcon }).bindPopup(makePopupYt(val));
-    marker.addTo(repeaters[overlay]);
+    marker = L.marker([lat, lon], { icon: selectedIcon }).bindPopup(makePopupSi(val));
+    repeaters[overlay].addLayer(marker);
+  }
+}
+
+mapYTFunc = function(val, overlay, repeaters) {
+  getCoordsFromAddress2(val["Location"]).then(function(coords) {
+    if (coords.features.length > 0) {
+      var marker;
+      var selectedIcon = blueIcon;
+      var [lon, lat] = coords.features[0].geometry.coordinates;
+      if (val.Modes.toLowerCase().includes("dstar")) { selectedIcon = greenIcon; }
+      if (val.Modes.toLowerCase().includes("tetra")) { selectedIcon = greyIcon; }
+      if (val.Modes.toLowerCase().includes("dmr")) { selectedIcon = orangeIcon; }
+      if (val.State == "inactive") { selectedIcon = redIcon; }
+      marker = L.marker([lat, lon], { icon: selectedIcon }).bindPopup(makePopupYt(val));
+      marker.addTo(repeaters[overlay]);
+     }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 }
 
 function makePopupYt(node) {
@@ -301,6 +297,19 @@ function makePopup2(node) {
   return popup;
 }
 
+function makePopupSi(node) {
+  let popup = '<div><b><i>' + node.ID + '</i></b><br><b>' + node["QTH"] + '</b><br><div>' + "" + '</div>' + '<ul style="padding-left: 8px;font-size: smaller;">';
+  $.each(node, function (k, v) {
+    if (k == 'Callsign' || k == 'QTH/Name' || k == 'banda' || k == 'description' || k == 'nameA' || k == 'overlay' || k == 'online' || k == 'pl_in' || k == 'pl_out') {
+
+    } else {
+      popup += '<li><b>' + k + '</b>: ' + v + '</li>';
+    }
+  });
+  popup += '</ul></div>';
+  return popup;
+}
+
 function getCoordsFromAddress(address) {
   const api = 'https://nominatim.openstreetmap.org/search?format=geojson&q=' + encodeURI(address);
   return new Promise((resolve) => {
@@ -316,30 +325,46 @@ function getCoordsFromAddress(address) {
 }
 
 function getCoordsFromAddress2(address) {
-  const api = 'https://nominatim.openstreetmap.org/search?format=geojson&limit=5&city=${'
-   + encodeURI(address) + '}';
-  $.ajax({
-      url: api,
-      method: 'GET',
-    })
-    .done(function (data) {
-      return data;
-      // if (data.length > 0) {
-      //   return data[0].geometry.coordinates;
-      // }
-      // return [null, null];
-    })
-    .fail(function (xhr) {
-      console.error(xhr);
+  const api = 'https://nominatim.openstreetmap.org/search?format=geojson&limit=5&city=${' + encodeURI(address) + '}';
+  return $.ajax({
+    url: api,
+    method: 'GET',
+    async: false
   });
-  // return new Promise((resolve) => {
-  //   fetch(api)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       resolve(data.features);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // });
 }
+
+$.getJSON({
+  url: "https://raw.githubusercontent.com/yo6nam/yodb/main/all.json",
+  cache: true,
+  crossDomain: true,
+  crossOriginIsolated: false
+}, function (data) {
+  // placeYoMarkers(data);
+  // placeLocatorMarkers(fm_markers_hu, "FM-HU");
+  // placeLocatorMarkers(dmr_markers_hu, "DMR-HU");
+  // placeLocatorMarkers(dstar_markers_hu, "DSTAR-HU");
+  // placeLocatorMarkers(dstar_markers_hu, "C4FM-HU");
+
+  const prom_yo = placeLocatorMarkers("YO-repeaters", mapYOFunc, Object.values(data.nodes).map((val) => val));//.then(function(repeaters) {
+    // addLayers2Map(repeaters);
+  // });
+  const prom_ha = placeLocatorMarkers("HA-repeaters", mapHuFunc, fm_markers_hu, dmr_markers_hu, dstar_markers_hu, c4fm_marker_hu);//.then(function(repeaters) {
+    // addLayers2Map(repeaters);
+  // });
+  const prom_si = placeLocatorMarkers("S5-repeaters", mapSiFunc, markers_2m_fm_s5, markers_70cm_fm_s5, markers_ecolink_s5);//.then(function(repeaters) {
+    // addLayers2Map(repeaters);
+  // });
+  // const prom_yt = placeLocatorMarkers("YT-repeaters", mapYTFunc, markers_YT);//.then(function(repeaters) {
+  //   addLayers2Map(repeaters);
+  // });
+  
+  Promise.all([prom_yo, prom_ha, prom_si]).then(function(repeaters) {
+    var all_repeaters = {};
+    repeaters.map((v) => {
+      $.each(v, function(key, val) {
+        all_repeaters[key] = val;
+      });
+    });
+    addLayers2Map(all_repeaters);
+  });
+});
